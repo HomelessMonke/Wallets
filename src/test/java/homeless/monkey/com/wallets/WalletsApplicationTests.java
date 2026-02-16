d679664780f7c37fd57d73ff3d6b0de9361b5f8c
HomelessMonkey <adikovwork@gmail.com>
2026-02-16 18:19:01 +0300

    Init project

---
+++ src/test/java/homeless/monkey/com/wallets/WalletsApplicationTests.java
@@ -0,0 +1,13 @@
+package homeless.monkey.com.wallets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
